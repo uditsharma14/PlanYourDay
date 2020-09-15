b3e032a4bc5db67d5df825eba649dbe141dea8ad
udit <udit.sharma@accuity.com>
2020-09-15 17:59:14 -0500

    Plan your Day.

---
+++ src/main/java/com/productivework/app/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.productivework.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
